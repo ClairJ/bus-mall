241491876cec17d65f070a75bff05ebe106e0ae7
Joel Clair <clair.joel@yahoo.com>
2017-10-03 12:55:38 -0700

    mon

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
-var clicks = 0;
+BusMall.clicks = 0;
 var totals = [];
 BusMall.allPics = [];
+BusMall.lastDisplayed = [];
 
-
+BusMall.resultsList = getElementById('pics')
 function BusMall(name, filepath) {
   this.name = name;
   this.filepath = filepath;
+  this.timesDisplayed = 0;
+
   BusMall.allPics.push(this);
 }
 
@@ -32,56 +35,99 @@ new BusMall('water-can', 'img/water-can.jpg');
 new BusMall('wine-glass', 'img/wine-glass.jpg');
 
 function randomProduct() {
-  var value1 = 0;
-  var value2 = 0;
-  var value3 = 0;
-  //pic1
-  function testing() {
-    for (var i = 0; i < 1; i++) {
-      var randomIndex1 = Math.floor(Math.random() * BusMall.allPics.length);
-      value1 += randomIndex1;
-      var randomIndex2 = Math.floor(Math.random() * BusMall.allPics.length);
-      value2 += randomIndex2;
-      var randomIndex3 = Math.floor(Math.random() * BusMall.allPics.length);
-      value3 += randomIndex3;
-    }
+  var imgEl = document.getElementById('pic1');
+  var imgEl2 = document.getElementById('pic2');
+  var imgEl3 = document.getElementById('pic3');
+
+  var value1 = Math.floor(Math.random() * BusMall.allPics.length);
+  var value2 = Math.floor(Math.random() * BusMall.allPics.length);
+  var value3 = Math.floor(Math.random() * BusMall.allPics.length);
+
+  //check the last displayed for duplicates
+  // while(BusMall.lastDisplayed.includes(value1) || BusMall.lastDisplayed.includes(value2) || BusMall.lastDisplayed.includes(value3) ||
+  // value1 === value2 ||
+  // value1 === value3 ||
+  // value2 === value3){
+  //
+  // }
+
+  while(value1 === value2 || value1 === value3 || value2 === value3) {
+    value1 = Math.floor(Math.random() * BusMall.allPics.length);
+    value2 = Math.floor(Math.random() * BusMall.allPics.length);
+    value3 = Math.floor(Math.random() * BusMall.allPics.length);
   }
-  if (value1 === value2 || value1 === value3 || value2 === value3) {
-    testing();
-  }
-  clicks += 1;
 
   console.log('clicks is ', clicks);
   console.log(value1);
-  var imgEl = document.getElementById('pic1');
-  imgEl.addEventListener('click', randomProduct);
   imgEl.src = BusMall.allPics[value1].filepath;
   console.log('pic # // value1 ',value1);
   //pic2
-  var imgEl2 = document.getElementById('pic2');
-  imgEl2.addEventListener('click', randomProduct);
   imgEl2.src = BusMall.allPics[value2].filepath;
   console.log('pic # // value2 ',value2);
   //pic3
-  var imgEl3 = document.getElementById('pic3');
-  imgEl3.addEventListener('click', randomProduct);
   imgEl3.src = BusMall.allPics[value3].filepath;
   console.log('pic # // value3 ',value3);
 
-  function productClicked1() {
-    totals.push(value1);
-  }
-  imgEl.addEventListener('click', productClicked1);
+  //inc
+  BusMall.allPics[value1].timesDisplayed += 1;
+  BusMall.allPics[value2].timesDisplayed += 1;
+  BusMall.allPics[value3].timesDisplayed += 1;
 
-  function productClicked2() {
-    totals.push(value2);
-  }
-  imgEl2.addEventListener('click', productClicked2);
+  BusMall.lastDisplayed[0] = value1;
+  BusMall.lastDisplayed[1] = value2;
+  BusMall.lastDisplayed[2] = value3;
+  //keep track
+  BusMall.lastDisplayed.unshift(value1, value2, value3);
 
-  function productClicked3() {
-    totals.push(value3);
-  }
-  imgEl3.addEventListener('click', productClicked3);
+
+
+  // function productClicked1() {
+  //   totals.push(value1);
+  // }
+  // imgEl.addEventListener('click', productClicked1);
+  //
+  // function productClicked2() {
+  //   totals.push(value2);
+  // }
+  // imgEl2.addEventListener('click', productClicked2);
+  //
+  // function productClicked3() {
+  //   totals.push(value3);
+  // }
+  // imgEl3.addEventListener('click', productClicked3);
   console.log(totals);
+  console.log('value1 is ',value1);
+  console.log('====================');
 }
+
+function handleClick(e) {
+  //track of number of total clicks
+  BusMall.clicks += 1;
+  //what to do when we hit 25 clicks
+  if(BusMall.clicks > 24) {
+    BusMall.section.removeEventListener('click',handleClick);
+
+    for (var i = 0; i < BusMall.allPics.length; i++) {
+      if(event.target.id === BusMall.allPics[i].name) {
+        BusMall.allPics[i].votes += 1;
+      }
+    }
+    //display the results
+    showResults();
+  }
+  //count votes for each image
+  // console.log('e.target', e.target);
+  //call randomProduct function again
+  //call randomProduct function again
+  randomProduct();
+}
+
+function showResults(); {
+  for(var i = 0; i < BusMall.allPics.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = BusMall.allPics[i].name + ' has ' + BusMall.allPics[i].votes + ' votes in ' + BusMall.allPics[i]timesDisplayed + ' times shown '
+    Busmall.allPics
+  }
+}
+BusMall.section.addEventListener('click', handleClick);
 randomProduct();
